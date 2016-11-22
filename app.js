e69b7f2881a05f61a165a9112469055fb7ac531c
Maarten de Graaf <wiejeben@gmail.com>
2016-11-22 14:47:37 +0100

    Separate SASS file from CSS folder

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index'),
+    users = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'resources'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true,
   sourceMap: true
