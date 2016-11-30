2b2a9bec4bf683daba09259b9112d7d7e8047d28
Maarten de Graaf <wiejeben@gmail.com>
2016-11-30 10:09:20 +0100

    Add levels frontpage

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let express = require('express'),
     bodyParser = require('body-parser');
 
 var index = require('./routes/index'),
-    users = require('./routes/users');
+    levels = require('./routes/levels');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/levels', levels);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
