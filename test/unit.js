11e3aeb9e116994722484120a22c9838781b43c6
Maarten de Graaf <wiejeben@gmail.com>
2017-01-06 21:03:31 +0100

    Clean unit test code

--- test/unit.js
+++ test/unit.js
@@ -1,27 +1,18 @@
-var test = require('unit.js');
+const test = require('unit.js');
 const User = require('./../services/User');
+const Result = require('./../services/Result');
+
 // http://unitjs.com/guide/quickstart.html
-describe('Unit tests', function(){
+describe('Unit tests', () => {
 
-    it('Only student numbers are allowed to log-in', function() {
-        var username = 'Unit123';
-        test.must(User.validateUsername(username)).be.false();
+    it('Only student numbers are allowed to log-in', () => {
+        test.must(User.validateUsername("Unit123")).be.false();
+        test.must(User.validateUsername("123")).be.true()
     });
 
-    // Unit test for checking if an answer is correctAnswer
-    it('TestCorrectAnswer', function() {
-      function _TestCorrectAnswer(userAnswer) {
-        var correctAnswer = 1;
-        var result;
-        if(correctAnswer == userAnswer) {
-           return true;
-        } else {
-          return false;
-        }
-      }
-
-      test.must(_TestCorrectAnswer(0)).be.false();
-      test.must(_TestCorrectAnswer(1)).be.true();
-    });
+    it('Check if answer is correct', () => {
+        test.must(Result.validateAnswer("Amsterdam", "amsterdam")).be.false();
+        test.must(Result.validateAnswer("Amsterdam", "Amsterdam")).be.true()
+    })
 
 });
