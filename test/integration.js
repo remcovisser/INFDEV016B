6da63d04b164107c29fbccdab3f65996a4dc6bdf
Maarten de Graaf <wiejeben@gmail.com>
2017-02-13 10:17:43 +0100

    Refactor integration test code

--- test/integration.js
+++ test/integration.js
@@ -1,33 +1,32 @@
-var Level = require('./../services/level'),
+const Level = require('./../services/level'),
     User = require('./../services/User'),
     Result = require('./../services/Result'),
-    root = require('./../app.js');
+    root = require('./../app.js'),
     test = require('unit.js');
 
-require('mongodb').MongoClient.connect('mongodb://localhost:27017/EnglishPractise', function(err, db) {
+require('mongodb').MongoClient.connect('mongodb://localhost:27017/EnglishPractise', (err, db) => {
     if (err) {
         console.error('Unable to connect to MongDB:');
-        throw new Error(err);
+        throw new Error(err)
     }
 
-describe('Integration tests', function(){
-    
-    it('Can we insert a new user?', function(done){
-        var user = new User();
-        user.authenticate("2030", function(rtn){
-            test.object(rtn);
-            done();
-        })
-    })
+    describe('Integration tests', () => {
 
-    it('Can we find results of this user?', function(done){
-
-        var resC = new Result();
-        var result = resC.findResults("2030", function(rtn){
-            test.object(rtn);
-            done();
+        it('Can we insert a new user?', done => {
+            const user = new User();
+            user.authenticate("2030", rtn => {
+                test.object(rtn);
+                done()
+            })
         });
 
-    });
-});
+        it('Can we find results of this user?', done => {
+
+            const resC = new Result();
+            resC.findResults("2030", rtn => {
+                test.object(rtn);
+                done()
+            })
+        })
+    })
 });
